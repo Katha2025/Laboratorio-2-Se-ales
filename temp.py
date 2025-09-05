8130f47b9b56b4989f72d7a9733644cfe02e809f
LucianaCarbone <est.luciana.carbone@unimilitar.edu.co>
2025-09-04 19:33:36 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,37 @@
+import nidaqmx                     # Librería daq. Requiere haber instalado el driver nidaqmx
+from nidaqmx.constants import AcquisitionType # Para definir que adquiera datos de manera consecutiva
+import matplotlib.pyplot as plt    # Librería para graficar
+import nidaqmx                     # Librería daq. Requiere haber instalado el driver nidaqmx
+from nidaqmx.constants import AcquisitionType # Para definir que adquiera datos de manera consecutiva
+import matplotlib.pyplot as plt    # Librería para graficar
+import numpy as np                 # Librería de funciones matemáticas
+
+#%% Adquisición de la señal por tiempo definido
+
+fs = 400           # Frecuencia de muestreo en Hz. Recordar cumplir el criterio de Nyquist
+duracion = 5        # Periodo por el cual desea medir en segundos
+senal = []          # Vector vacío en el que se guardará la señal
+dispositivo = 'Dev1/ai0' # Nombre del dispositivo/canal (se puede cambiar el nombre en NI max)
+
+total_muestras = int(fs * duracion)
+
+with nidaqmx.Task() as task:
+    # Configuración del canal
+    task.ai_channels.add_ai_voltage_chan(dispositivo)
+    # Configuración del reloj de muestreo
+    task.timing.cfg_samp_clk_timing(
+        fs,
+        sample_mode=AcquisitionType.FINITE,   # Adquisición finita
+        samps_per_chan=total_muestras        # Total de muestras que quiero
+    )
+
+    # Lectura de todas las muestras de una vez
+    senal = np.array(task.read(number_of_samples_per_channel=total_muestras))
+
+t = np.arange(len(senal))/fs # Crea el vector de tiempo 
+plt.plot(t,senal)
+plt.axis([0,duracion,min(senal),max(senal)])
+plt.grid()
+plt.title(f"fs={fs}Hz, duración={duracion}s, muestras={len(senal)}")
+plt.show()
+np.savetxt('Senal_lab_2.txt', [t,  senal])
